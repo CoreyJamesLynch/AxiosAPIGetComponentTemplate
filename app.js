d4e8077e42724f8a4663d3daeeacd08f206b9b0b
bearinawolfpack <bearinawolfpack@gmail.com>
2021-06-04 13:01:05 -0400

    Display input values to user
    Add blank UL to index.html
    Create LI names newShow
    Add Input value to newShow
    Append newShow to blank UL "addShow"

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const searchButton = document.querySelector('#searchButton');
 const searchInput = document.querySelector('#searchInput');
+const addShow = document.querySelector('#addShow')
 
 searchButton.addEventListener('click', (event) => {
   event.preventDefault();
+  const newShow = document.createElement('li')
+  newShow.append(searchInput.value)
+  addShow.append(newShow)
   console.log(searchInput.value);
 });
+
+// append input values as LI elements by adding blank UL to HTML
