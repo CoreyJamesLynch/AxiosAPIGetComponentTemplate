479acb2db67737209b1c8849a71ac5c997398ae2
bearinawolfpack <bearinawolfpack@gmail.com>
2021-06-04 12:50:31 -0400

    Create button input logic
    On button click obtain input value
    Input value is required

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert('App.js is connected!')
+const searchButton = document.querySelector('#searchButton');
+const searchInput = document.querySelector('#searchInput');
+
+searchButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log(searchInput.value);
+});
