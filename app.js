ff17fcb1dd9188210c8f2088c59531e9e0e18b28
bearinawolfpack <bearinawolfpack@gmail.com>
2021-06-07 09:27:49 -0400

    Display data unique to user input on the page for the user to see

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const addShow = document.querySelector('#addShow');
 searchButton.addEventListener('click', (event) => {
   event.preventDefault();
   if (!addShow.childNodes[0]) {
-    displayShows();
+    getShows();
   } else {
     removeShows(addShow.childNodes[0]);
-    displayShows();
+    getShows();
   }
 });
 
-const displayShows = () => {
+const displayShows = (showImage) => {
+  console.log("The current show image is", showImage)
   const newShow = document.createElement('li');
-  newShow.append(getShows(searchInput.value));
+  newShow.append(showImage);
   addShow.append(newShow);
 };
 
@@ -22,12 +23,18 @@ const removeShows = (shows) => {
   shows.remove();
 };
 
-const getShows = async (show) => {
+const getShows = async () => {
   try {
-    const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${show}`);
-    console.log('Success, here is your request', response);
+    const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchInput.value}`);
+
+    for(show in response.data){
+      const showImageMed = response.data[show].show.image.medium;
+      const showName = response.data[show].show.name;
+      console.log(`Here is the image for ${showName} ->`, showImageMed)
+      displayShows(showImageMed)
+    }
   } catch (error) {
     console.log("Something went wrong, here's the error", error);
   }
 };
-// use input value to query api
+// Display data unique to user input to the page for the user to see
